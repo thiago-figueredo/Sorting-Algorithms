1f447f714184ab97596f1be7bf5dfc17bb6c96bf
Thiago <thiago10fca@hotmail.com>
2021-05-16 09:30:21 -0300

    Implemented Selection Sort

---
+++ printArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+
+void printArray (T &arr, int n) {
+  for (int i = 0; i < n; i++) {
+    int lastIndex = n-1;
+    auto element = arr[i];
+    i == lastIndex ? cout << element << '\n' : cout << element << ',';
+  }
+}
---
+++ types.h
@@ -0,0 +1,9 @@
+enum Type {
+  Char = 1,
+  Int = 4,
+};
+
+enum Bool {
+  False = 0,
+  True = 1,
+}
